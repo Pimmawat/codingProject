51cd8fa623b083cebaa1324f3d22a7a29206da00
PhuKub. <118192072+Pimmawat@users.noreply.github.com>
2024-12-14 21:02:33 +0700

    Update database.js

--- server/database.js
+++ server/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+require('dotenv').config();
 
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root', 
-    password: '', 
-    database: 'footballdb1', 
-  });
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+});
 
 module.exports = db;
